3952cd9ad0051a1632494c0d8f9ca5b395df231a
Horror-king <hassanadvice2@gmail.com>
2024-07-31 12:20:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const accessToken = 'key-50qe9h1Sy5NA9xN9HO3gkKNp2U90djP0ECejJ7h4gpUDDVzUPDv8olzskt9jaLk4LXgNoPN9fbv4xXBMG1zXZD39fuwu9Bjc'; // Replace with your actual access token
+const accessToken = 'key-2UXkxixdf6UGEpw4JYJ7iHJZ8tPnSi3Aq3drDbO9KJrrhixHtj2MEgG5UgAqkB9vRKyAFiqBbThnowsaJoG83fxuAzDpHGmQ'; // Replace with your actual access token
 
 app.use(cors()); // Enable CORS
 app.use(express.json()); // Parse JSON bodies
@@ -20,7 +20,7 @@ app.get('/tr', async (req, res) => {
   try {
     const response = await axios.post(
       'https://api.getimg.ai/v1/essential-v2/text-to-image',
-      { prompt: prompt },
+      { prompt: prompt, num_images: 1 }, // Request only one image
       {
         headers: {
           'Authorization': `Bearer ${accessToken}`,
